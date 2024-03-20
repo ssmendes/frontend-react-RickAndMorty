64a3a90a73743598f862cd7347838e507684ee83
Stella <53915799+ssmendes@users.noreply.github.com>
2024-03-20 07:40:24 -0300

    Update Pagination.jsx

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -1,35 +1,20 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import './Pagination.css'
-//import Vector from '../../assets/vector-svg.svg'
 
-// const Pagination = ({ currentPage, totalPages, prevPage, nextPage }) => {
-
-//   return (
-//     <div className="pagination">
-//       <button onClick={prevPage} disabled={currentPage === 1}>
-//         <img src={Vector} alt="Previous Page" className="prev-page"/>
-//       </button>
-//       <button onClick={nextPage} disabled={currentPage === totalPages}>
-//         <img src={Vector} alt="Next Page" className="next-page" />
-//       </button>
-//     </div>
-//   );
-// }
-
-const Pagination = ({ currentPage, totalPages, onNextPage, onPageChange }) => {
+const Pagination = ({ currentPage, total_pages, onNextPage, onPageChange }) => {
   // Array de números de página para exibição
   const pageNumbers = [];
   const maxButtons = 5;
 
   //gera os números de página a serem exibidos
-  for (let i = 1; i <= totalPages; i++) {
+  for (let i = 1; i <= total_pages; i++) {
     pageNumbers.push(i);
   }
 
   //lógica para exibir apenas um número limitado de botões de pagina
   let startIndex = Math.max(currentPage - Math.floor(maxButtons / 2), 1);
-  let endIndex = Math.min(startIndex + maxButtons - 1, totalPages);
+  let endIndex = Math.min(startIndex + maxButtons - 1, total_pages);
 
   if (endIndex - startIndex + 1 < maxButtons){
     startIndex = Math.max(endIndex - maxButtons + 1, 1);
@@ -56,7 +41,7 @@ const Pagination = ({ currentPage, totalPages, onNextPage, onPageChange }) => {
       ))}
 
       {/* Botão de próxima página */}
-      <button onClick={onNextPage} disabled={currentPage === totalPages}>
+      <button onClick={onNextPage} disabled={currentPage === total_pages}>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#828282ac">
           <path d="M0 0h24v24H0z" fill="none"/>
           <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/>
